399d0697113fb38b744feb9bc9910fc5f4eb2c5b
Maggie Asare <59848678+adwoama@users.noreply.github.com>
2022-04-10 17:17:02 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import networkx as nx
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+H = nx.read_gml("football.gml") #read in a gml file from working directory
+nx.write_graphml_lxml(H, "football.graphml") #write out a grpahml file to working directory
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
